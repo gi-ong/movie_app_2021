8f84a606b55000f663f497ccac13e8c240a71fd9
gi-ong <pugo9165@gmail.com>
2021-09-15 19:07:27 +0900

    add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
-import { HashRouter } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
+import Detail from "./routes/Detail";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <HashRouter>
-      <Route path="/" component={Home} />
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
+      <Route path="/movie/:id" component={Detail} />
     </HashRouter>
   );
 }
